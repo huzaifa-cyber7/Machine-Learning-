c9c33e5aee3ec6466e834e844e2b6741807012e3
Huzaifa <huzaifausman929@gamil.com>
2025-09-19 18:02:21 -0500

    Add my code for Machine Learning.py

--- Machine Learning.py
+++ Machine Learning.py
@@ -1,1 +1,9 @@
-print("hello")
+target=220
+guess=None
+while guess !=target:
+    guess=int(input("enter number"))
+    if guess==target:
+        print("target achieved")
+    else:
+        print("target not achieved")
+
